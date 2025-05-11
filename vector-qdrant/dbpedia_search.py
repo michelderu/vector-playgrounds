61ae931af5d18aa42dd048260014e2909180d545
Michel de Ru <michel@de-ru.net>
2025-05-11 13:08:46 +0200

    IMproved search order

--- vector-qdrant/dbpedia_search.py
+++ vector-qdrant/dbpedia_search.py
@@ -20,7 +20,7 @@ sparse_model = SparseTextEmbedding(
 sparse_vector = list(sparse_model.embed(query_text))[0]
 
 ### search (deprecated): Dense vector with Binary Quantization + Filtering
-print ("### Search: Dense vector with Binary Quantization ###")
+print ("### Search: Dense vector with Binary Quantization + Filtering ###")
 
 start_time = time.perf_counter()
 results = client.search(
@@ -53,7 +53,7 @@ for result in results:
 print(f"  Time taken to process results: {(end_time - start_time)*1000:.2f}ms")
 
 ### query_points: Dense vector + Filtering
-print ("\n### Query points: Dense vector ###")
+print ("\n### Query points: Dense vector + Filtering ###")
 
 start_time = time.perf_counter()
 results = client.query_points(
@@ -82,7 +82,7 @@ for result in results.points:
 print(f"  Time taken to process results: {(end_time - start_time)*1000:.2f}ms")
 
 ### query_points: Sparse vector + Filtering
-print ("\n### Query points: Sparse vector ###")
+print ("\n### Query points: Sparse vector + Filtering ###")
 
 start_time = time.perf_counter()
 results = client.query_points(
@@ -111,7 +111,7 @@ for result in results.points:
 print(f"  Time taken to process results: {(end_time - start_time)*1000:.2f}ms")
 
 ### query_points: Dense vector with Binary Quantization + Filtering
-print ("\n### Query points: Dense vector with Binary Quantization ###")
+print ("\n### Query points: Dense vector with Binary Quantization + Filtering ###")
 
 start_time = time.perf_counter()
 results = client.query_points(
@@ -147,18 +147,18 @@ for result in results.points:
 print(f"  Time taken to process results: {(end_time - start_time)*1000:.2f}ms")
 
 ### query_points: Reciprocal Rank Fusion with Dense vector + Sparse vector + Filtering
-print ("\n### Query points: Reciprocal Rank Fusion with Dense vector + Sparse vector ###")
+print ("\n### Query points: Reciprocal Rank Fusion with Dense vector + Sparse vector + Filtering ###")
 
 prefetch = [
     models.Prefetch(
       query=dense_vector,
       using="dense",
-      limit=20,
+      limit=20
     ),
     models.Prefetch(
       query=models.SparseVector(**sparse_vector.as_object()), # unpack dictionary into keyword arguments
       using="sparse",
-      limit=20,
+      limit=20
     )
 ]
 
@@ -171,6 +171,7 @@ results = client.query_points(
     ),
     with_payload=True,
     with_vectors=False,
+    # Trying the filter AFTER prefetching the results to see performance impact
     query_filter=models.Filter(
         must=[
             models.FieldCondition(
@@ -191,14 +192,36 @@ for result in results.points:
 print(f"  Time taken to process results: {(end_time - start_time)*1000:.2f}ms")
 
 ### query_points: Reciprocal Rank Fusion with Dense vector and Binary Quantization + Sparse vector + Filtering
-print ("\n### Query points: Reciprocal Rank Fusion with Dense vector + Sparse vector and Binary Quantization ###")
+print ("\n### Query points: Reciprocal Rank Fusion with Dense vector and Binary Quantization + Sparse vector + Filtering ###")
 
 prefetch = [
+    # Prefetch the dense vector from RAM using Binary Quantization while filtering the candidates using the user_id
     models.Prefetch(
       query=dense_vector,
       using="dense",
       limit=20,
+      # Prefetch from RAM using Binary Quantization
+      params=models.SearchParams(
+        quantization=models.QuantizationSearchParams(
+            # Avoid rescoring in prefetch
+            # We should do it explicitly on the second stage
+            # We oversample to get more results that later get filtered down using the dense vector from disk
+            ignore=False,
+            rescore=False,
+            oversampling=3.0
+        )
+      ),
+      # Filter the candidates using the user_id
+      filter=models.Filter(
+        must=[
+            models.FieldCondition(
+                key='user_id',
+                match=models.MatchValue(value=random.randint(1, 10))
+            )
+        ]
+      )
     ),
+    # Prefetch the sparse vector from RAM
     models.Prefetch(
       query=models.SparseVector(**sparse_vector.as_object()), # unpack dictionary into keyword arguments
       using="sparse",
@@ -209,30 +232,21 @@ prefetch = [
 start_time = time.perf_counter()
 results = client.query_points(
     collection_name="dbpedia_entities_openai3",
+    # Use prefetch to get the results from RAM
     prefetch=prefetch,
+    # In the second stage, we rescore the results using the dense vector from disk
+    search_params=models.SearchParams(
+        quantization=models.QuantizationSearchParams(
+            rescore=True,
+        )
+    ),
+    # Use RRF to fuse the results
     query=models.FusionQuery(
         fusion=models.Fusion.RRF,
     ),
     with_payload=True,
     with_vectors=False,
-    limit=5,
-    search_params=models.SearchParams(
-        quantization=models.QuantizationSearchParams(
-            ignore=False,
-            rescore=True,
-            oversampling=3.0,
-        )
-    ),
-    query_filter=models.Filter(
-        must=[
-            models.FieldCondition(
-                key='user_id',
-                match=models.MatchValue(
-                    value=random.randint(1, 10)
-                )
-            )
-        ]
-    )
+    limit=5
 )
 end_time = time.perf_counter()
 
